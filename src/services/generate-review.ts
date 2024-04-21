07dd6736c542cc90c031cdc2713035bf23b0db6d
lucasgianine <lucas.esilva@sptech.school>
2024-04-21 12:51:15 -0300

    Melhora prompt de instrução e chama função de comentar

--- src/services/generate-review.ts
+++ src/services/generate-review.ts
@@ -1,11 +1,12 @@
 import { openai } from '../lib/openai'
 import { inputExample, outputExample } from '../types/example'
+import postComment from './post-comment-github'
 
-async function generateReview (codeToReview: string): Promise<string | null> {
+async function generateReview(codeToReview: string): Promise<void> {
   const completion = await openai.chat.completions.create({
     messages: [{
       role: 'user',
-      content: 'Agora você é um assistente sênior em programação que entende TypeScript, ReactJS, HTML, CSS e Java. Você receberá códigos e através deles você vai escolher os mais importantes e irá gerar uma revisão detalhada passando o resumo daquele código e uma sugestão de melhoria (caso tenha). Você irá ajudar o desenvolvedor a aprender com o que ele errou. Não esqueça de mostrar a linha e o arquivo que foi tirado a sugestão'
+      content: 'Agora você é um assistente sênior em programação que entende TypeScript, ReactJS, HTML, CSS e Java. Você irá ajudar o desenvolvedor a aprender com o que ele errou. Não esqueça de mostrar a linha e o arquivo que foi tirado a sugestão'
     },
     {
       role: 'assistant',
@@ -13,9 +14,9 @@ async function generateReview (codeToReview: string): Promise<string | null> {
     },
     {
       role: 'user',
-      content: `Certo, aqui vai o código que você revisará, quero que traga SUGESTÃO, RESUMO e que me ENSINE com o que você me sugeriu, caso meu código esteja certo, elogie.:
+      content: `Certo, aqui vai o código que você revisará, quero que traga SUGESTÃO, RESUMO e que me ENSINE com o que você me sugeriu, caso meu código esteja certo, elogie:
 
-      as respostas devem ser em TÓPICOS
+      As respostas devem ser em TÓPICOS.
       """
       ${inputExample}
       """`
@@ -26,20 +27,19 @@ async function generateReview (codeToReview: string): Promise<string | null> {
     },
     {
       role: 'user',
-      content: `Obrigado, assistente, obrigado por esse insight, exatamente dessa forma que eu queria que você revisasse, continue assim, você está me ajudando muito!
+      content: `Obrigado, assistente! Exatamente dessa forma que eu queria que você revisasse, continue assim, você está me ajudando muito!
       Estou com um outro código MUITO IMPORTANTE para que você revise para mim:
       """
       ${codeToReview}
       """`
     }],
     temperature: 0.4,
-    model: 'gpt-3.5-turbo',
-
+    model: 'gpt-3.5-turbo'
   })
 
   const response = completion.choices[0].message.content
 
-  return response
+  postComment(response)
 }
 
 export default generateReview
