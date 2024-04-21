0556fb700f5b5dec4a44916151a0ae30e81877f0
lucasgianine <lucas.esilva@sptech.school>
2024-04-21 12:49:51 -0300

    Cria função para iterar código dos commits

---
+++ src/services/create-code-to-review.ts
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import generateReview from './generate-review'
+
+async function createCodeToReview(commits: any, headers: any, params: any) {
+  let codeToReview = ''
+
+  for (const commit of commits) {
+    const url = `https://api.github.com/repos/${params.user}/${params.repo}/commits/${commit.sha}`
+    const response = (await axios.get(url, { headers })).data
+
+    codeToReview = response.files.map((file: any) =>
+      `Arquivo: ${file.filename}
+      Código: """
+      ${file.patch}
+      """`
+    ).join('\n\n')
+  }
+
+  generateReview(codeToReview)
+}
+
+export default createCodeToReview
