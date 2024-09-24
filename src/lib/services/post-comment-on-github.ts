8d38d7d755129a726d084937f4a86b45f74e1a24
lucasgianine <lucas.esilva@sptech.school>
2024-09-24 02:06:22 -0300

    Cria service único para postar comentário no Github

---
+++ src/lib/services/post-comment-on-github.ts
@@ -0,0 +1,29 @@
+import githubClient from '../clients/github-client'
+import { Commits, File, FilesChanged } from '../types/commits'
+import generateReview from './generate-review'
+
+export async function postCommentOnGithub() {
+  try {
+    const commits: Commits[] = (await githubClient.getCommits()).data
+
+    let codeToReview = ''
+
+    for (const commit of commits) {
+      const response: FilesChanged = (await githubClient.getFilesChanged(commit.sha)).data
+      codeToReview = response.files.map((file: File) =>
+        `Arquivo: ${file.filename}
+        Código: """
+        ${file.patch}
+        """`
+      ).join('\n\n')
+    }
+
+    const response = await generateReview(codeToReview)
+
+    await githubClient.postComment(response)
+
+    return 'Comentário postado com sucesso!'
+  } catch (err: any) {
+    console.error(err)
+  }
+}
